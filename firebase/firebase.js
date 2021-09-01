c5b35ae50d7f1222de48497d13a260903771cce6
Luis Andrade <luisfer220900@gmail.com>
2021-09-01 17:48:23 -0400

    first commit

---
+++ firebase/firebase.js
@@ -0,0 +1,42 @@
+import app from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+
+import firebaseConfig from './config'
+
+class Firebase {
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(firebaseConfig)
+    }
+    this.auth = app.auth()
+    this.db = app.firestore()
+    this.storage = app.storage()
+  }
+
+  // Registra un usuario
+  async registrar(nombre, email, password) {
+    const nuevoUsuario = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    )
+
+    return await nuevoUsuario.user.updateProfile({
+      displayName: nombre,
+    })
+  }
+
+  // Inicia sesión del usuario
+  async login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  // Cierra la sesión del usuario
+  async cerrarSesion() {
+    await this.auth.signOut()
+  }
+}
+
+const firebase = new Firebase()
+export default firebase
